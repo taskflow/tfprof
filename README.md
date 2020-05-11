f287e86f06a073fc6a4b0d96a069a790956bcab2
Tsung-Wei Huang <twh760812@gmail.com>
2020-05-11 15:36:48 -0600

    updated readme

--- README.md
+++ README.md
@@ -3,8 +3,23 @@
 TFProf provides the visualization and tooling needed for profiling 
 cpp-taskflow programs 
 
-The page is under construction.
+# Profile Your Cpp-Taskflow Program
 
+Generate tfprof data (.tfp) from your cpp-taskflow program through the python script:
+
+```bash
+~$ python3 tfprof.py -o output.tfp /path/to/my/cpp-taskflow/program arg1 arg2
+```
+
+Paste the content of `output.tfp` to the JSON text area at the following page:
+
+<p align="center">
+   <a href="https://cpp-taskflow.github.io/tfprof/">
+     <img width="100%" src="images/mainboard.png">
+   </a>
+</p>
+
+You may also open [index.html](index.html) from your browser to visualize tfprof data.
 
 
 
--- tfprof.py
+++ tfprof.py
@@ -1,69 +1,91 @@
 # program: tfprof
 
+import logging as logger
 import time
+import sys
 import json
 import argparse
 import os
 import tempfile
 import subprocess
 
-# parse the input arguments
-parser = argparse.ArgumentParser();
+def main():
 
-parser.add_argument(
-  '-o', '--output',
-  type=str,
-  help='file name to save the profiler result',
-  default="output.tfp"
-)
-
-parser.add_argument('program', nargs=argparse.REMAINDER)
-
-args = parser.parse_args();
-
-tfpb = time.perf_counter(); 
-
-print("profiling program:", ' '.join(args.program))
-
-with open(args.output, "w") as ofs:
-
-  ofs.write('[');
-
-  with tempfile.TemporaryDirectory() as dirname:
+  # configure logger
+  logger.basicConfig(
+    #format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+    format='%(asctime)s %(levelname)s: %(message)s', 
+    datefmt='%m/%d/%Y %I:%M:%S',
+    level=logger.DEBUG
+  )
   
-    prefix = os.path.join(dirname, 'executor-')
+  # parse the input arguments
+  parser = argparse.ArgumentParser();
   
-    os.environ["TF_ENABLE_PROFILER"] = prefix;
+  parser.add_argument(
+    '-o', '--output',
+    type=str,
+    help='file to save the result (default output.tfp)',
+    default="output.tfp"
+  )
+  
+  parser.add_argument(
+    'program', 
+    nargs=argparse.REMAINDER,
+    help='program to profile (e.g., path/to/binary args)'
+  )
+  
+  args = parser.parse_args();
+  
+  if(len(args.program) == 0) :
+    logger.error("no program specified");
+    sys.exit(1);
+  
+  tfpb = time.perf_counter(); 
+  
+  logger.info("profiling program \"" + ' '.join(args.program) +'\"')
+  
+  with open(args.output, "w") as ofs:
+  
+    ofs.write('[');
+  
+    with tempfile.TemporaryDirectory() as dirname:
     
-    prob = time.perf_counter();
-    subprocess.call(args.program);
-    proe = time.perf_counter();
-    print(f"program finished in {(proe - prob)*1000:0.2f} milliseconds")
-
-    executor_id = 0;
-    print('collecting profiled data ...')
-    for fnum, fname in enumerate(os.listdir(dirname)):
-      if fname.endswith(".tfp") : 
-        ifile = os.path.join(dirname, fname);
-        print("[%d] -> %s" % (fnum, ifile));
-        with open(ifile, "r") as ifs:
-          data = json.load(ifs);
-          data['executor'] = "executor " + str(executor_id);
-          executor_id = executor_id + 1;
-          if fnum != 0 :
-            ofs.write(',');
-          #ofs.write(ifs.read());
-          json.dump(data, ofs)
-
-  ofs.write(']');
-
-print("saved result to", args.output);
-
-tfpe = time.perf_counter(); 
+      prefix = os.path.join(dirname, 'executor-')
     
-print(f"tfprof finished in {(tfpe - tfpb)*1000:0.2f} milliseconds")
+      os.environ["TF_ENABLE_PROFILER"] = prefix;
+      
+      prob = time.perf_counter();
+      subprocess.call(args.program);
+      proe = time.perf_counter();
+      logger.info(f"program finished in {(proe - prob)*1000:0.2f} milliseconds")
+  
+      executor_id = 0;
+      logger.info("collecting profiled data ...")
+      for fnum, fname in enumerate(os.listdir(dirname)):
+        if fname.endswith(".tfp") : 
+          ifile = os.path.join(dirname, fname);
+          logger.info(f"processing {ifile:s} ...")
+          with open(ifile, "r") as ifs:
+            data = json.load(ifs);
+            data['executor'] = "executor " + str(executor_id);
+            executor_id = executor_id + 1;
+            if fnum != 0 :
+              ofs.write(',');
+            #ofs.write(ifs.read());
+            json.dump(data, ofs)
+  
+    ofs.write(']');
+  
+  logger.info(f"saved result to {args.output:s}");
+  
+  tfpe = time.perf_counter(); 
+      
+  logger.info(f"tfprof finished in {(tfpe - tfpb)*1000:0.2f} milliseconds")
 
-
+# main entry
+if __name__ == "__main__":
+  main();
 
 
 
